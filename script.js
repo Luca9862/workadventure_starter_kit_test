219afeb560a5e445ca694e88f5c42d4290d0ca89
Luca Canali <luca.canali@elmec.it>
2025-04-10 17:11:11 +0200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 console.log("hello");
 
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+WA.chat.sendChatMessage('Benvuto su WorkAdventure', 'Mr Robot');
+
+import {levelUp} from "@workadventure/quests";
+
+console.log("test_quest")
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+
+
+    console.log("Page Open")
+    sleep(5000)
+console.log("Assign 10 xp once")
+WA.player.levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
